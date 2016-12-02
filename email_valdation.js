074e0e832c61cadf5ad25fbf089db91044cb74f1
Nisar <noobards@users.noreply.github.com>
2016-12-02 11:03:32 +0530

    First Version

---
+++ email_valdation.js
@@ -0,0 +1,10 @@
+/*
+- Returns true if email address is valid
+- Returns false if email address is not valid
+*/
+
+function isEmailValid(str)
+{
+    var re = new RegExp('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/');
+    return re.test(str);
+}
